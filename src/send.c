08c8196e36ec25a1a029cb67bb784b6f65cc0d35
0x36D76289 <71098898+0x36D76289@users.noreply.github.com>
2026-04-23 06:07:15 +0200

    Refactor ICMP packet sending logic in send.c

--- src/send.c
+++ src/send.c
@@ -12,36 +12,24 @@ void send_packet(t_ping *ping)
     icmp_hdr = (struct icmphdr *)packet;
     data = packet + sizeof(struct icmphdr);
 
-    // Fill ICMP header
     icmp_hdr->type = ICMP_ECHO;
     icmp_hdr->code = 0;
     icmp_hdr->un.echo.id = ping->ident;
     icmp_hdr->un.echo.sequence = ping->seq_num++;
-    icmp_hdr->checksum = 0; // Calculate checksum later
+    icmp_hdr->checksum = 0;
 
-    // Fill data (optional, but common practice)
     snprintf(data, DATA_SIZE, "ft_ping data seq=%d", icmp_hdr->un.echo.sequence);
-
-    // Calculate checksum *after* filling the data
     icmp_hdr->checksum = checksum((unsigned short *)packet, PACKET_SIZE);
     gettimeofday(&ping->send_time, NULL);
-
     ssize_t bytes_sent = sendto(ping->sockfd, packet, PACKET_SIZE, 0,
                                 (struct sockaddr *)&ping->dest_addr, sizeof(ping->dest_addr));
     if (bytes_sent < 0)
     {
         if (errno == EAGAIN || errno == EWOULDBLOCK)
-        {
-            // Timeout. No packet will be sent in this case.
             printf("Request timeout for icmp_seq %d\n", icmp_hdr->un.echo.sequence);
-        }
         else
-        {
             perror("ft_ping: sendto");
-        }
     }
     else
-    {
         ping->packets_sent++;
-    }
 }
